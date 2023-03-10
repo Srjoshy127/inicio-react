eeea67af1f7f26fff59aff5dbb9c886d13a21c87
SrDiefer <94631654+SrDiefer@users.noreply.github.com>
2023-03-10 14:14:24 -0300

    anda a un curso de photoshop maldito asmatico

---
+++ app1.js
@@ -0,0 +1,4 @@
+//Hola
+//Esto es solo un comentario para decirte que sos bien maje
+//Necesitas un curso de programacion con Objetos, un curso de logica de programacion, un curso de bartender a y por si acaso
+//Un curso de Bisutería
